d7569a59cddc1c44d97af87aed18b0b579d8aa1e
mustafa <mustafa.gunes1702@gmail.com>
2016-04-15 00:09:53 +0300

    hibernate mapping for Kiosk

--- LibraryInformationSystem/src/java/model/Student.java
+++ LibraryInformationSystem/src/java/model/Student.java
@@ -1,12 +1,40 @@
 package model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
 /**
  *
  * @author ercan
  */
-public class Student {
+@Entity
+@Table(name = "student")
+@PrimaryKeyJoinColumn(name = "mail")
+public class Student extends User implements Serializable{
+    @Column(name = "school_no", nullable = false, unique = true)
     private String schoolNo;
+    @Column(name = "department", nullable = false)
     private String department;
-    private static int MAX_LIMIT;
+    
+    //private static int MAX_LIMIT;
+
+    public String getSchoolNo() {
+        return schoolNo;
+    }
+
+    public void setSchoolNo(String schoolNo) {
+        this.schoolNo = schoolNo;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
     
 }
