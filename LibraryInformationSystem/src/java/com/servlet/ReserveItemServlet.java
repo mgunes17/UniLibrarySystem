e4704a41ddcb84c827667091cdd69813885537a5
ercaany <ercaany@gmail.com>
2016-05-09 22:50:33 +0300

    update
    servlets updated

--- LibraryInformationSystem/src/java/com/servlet/ReserveItemServlet.java
+++ LibraryInformationSystem/src/java/com/servlet/ReserveItemServlet.java
@@ -6,22 +6,23 @@
 package com.servlet;
 
 import com.dao.HibernateSession;
+import com.model.Item;
+import com.model.ItemOperation;
+import com.model.ItemReservation;
+import com.model.SmartCard;
+import com.model.User;
+import com.model.UserType;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.model.Item;
-import com.model.ItemOperation;
-import com.model.SmartCard;
-import com.model.User;
-import com.model.UserType;
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import javax.servlet.http.HttpSession;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -31,9 +32,9 @@ import org.hibernate.Transaction;
  *
  * @author ercan
  */
-@WebServlet(name = "BorrowServlet", urlPatterns = {"/borrowitemservlet"})
-public class BorrowItemServlet extends HttpServlet {
-
+@WebServlet(name = "ReserveItemServlet", urlPatterns = {"/reserveitemservlet"})
+public class ReserveItemServlet extends HttpServlet {
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
@@ -57,17 +58,23 @@ public class BorrowItemServlet extends HttpServlet {
         "from User where smartCard = "+cardNo);
         List<User> result = query.list();
         
+        
+        Query query2 = session.createQuery(
+        "from ItemReservation where state = 5");
+        List<ItemReservation> result2 = query2.list();
+        
+        
         User user = (User) session.get(User.class, result.get(0).getMail());
         UserType userType = (UserType) session.get(UserType.class, user.getUserType());
-
+        
         if(smartCard == null){ // checking if the smartcard exists
             httpsession.setAttribute("state", 0); // state 0 means smartcard is not invalid
         }
         else if(item == null){ // checking if the item exists
             httpsession.setAttribute("state", 1); // state 1 means item is not invalid
         }
-        else if(item.getState() != 0){ // checking if the item is free
-            httpsession.setAttribute("state", 2); // state 2 means item is not free
+        else if(!result2.isEmpty()){ // checking if the item is free
+            httpsession.setAttribute("state", 10); // state 10 means item is already reserved
         }
         else if(user.getBorrowedItemCount() == userType.getMaxLimit()){ // make it parametric . checking if the user has the maxItemCount 
             httpsession.setAttribute("state", 3); // state 3 means user has the maxItemCount
@@ -76,31 +83,17 @@ public class BorrowItemServlet extends HttpServlet {
             httpsession.setAttribute("state", 8); // state 5 means user has no money
         }
         else{
-            Calendar c=new GregorianCalendar();
-            if(user.getUserType() == 2)
-                c.add(Calendar.DATE, 90);
-            else
-                c.add(Calendar.DATE, 30);
-            
-            Date date = c.getTime();
-            
-            ItemOperation io = new ItemOperation();
-            io.setItemNo(itemNo);
-            io.setMail(user.getMail());
-            io.setBorrowedDate(new Timestamp(new Date().getTime()));
-            io.setExpireDate(new Timestamp(date.getTime()));
-            
-            item.setCurrentUser(user.getMail());
-            item.setState(2);
-            
-            user.setBorrowedItemCount(user.getBorrowedItemCount() + 1 );
-            
+            ItemReservation ir = new ItemReservation();
+            ir.setItemNo(itemNo);
+            ir.setMail(user.getMail());
+            ir.setState(5);
+                        
             session.saveOrUpdate(user);
-            session.saveOrUpdate(item);
-            session.save(io);
+            session.save(ir);
             tx.commit();
             httpsession.setAttribute("state", 4); // state 4 means item is available and card is valid. borrow operation will be processed.
         }
         response.sendRedirect("kiosk.jsp");
     }
+
 }
